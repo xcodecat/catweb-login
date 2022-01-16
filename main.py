17b96ec3bdf0d2bded5b89483711e167a1f0d5f3
xcodecat <cheetah@cat.de>
2022-01-16 11:03:51 +0100

    .

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import wire
+from wire import Wire, Request
+from wire.static import StaticFiles
+from wire.templating import FoxTemplates
 
+app = Wire()
+app.mount(StaticFiles("./public"), "/public") 
+
+@app.get('/test')
+async def index():
+    return 'Hello World!'
