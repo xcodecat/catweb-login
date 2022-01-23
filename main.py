8a3e29ed4a7d03767bf42870d9c78065bbfdea8a
xcodecat <cheetah@cat.de>
2022-01-23 14:34:19 +0100

    .

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from wire import Wire, Request
 from wire.static import StaticFiles
 from wire.templating import FoxTemplates
+from wire.response import HTMLResponse
 
 app = Wire()
 app.mount(StaticFiles("./public"), "/public") 
 
-@app.get('/test')
-async def index():
-    return 'Hello World!'
+@app.get('/login')
+async def login(req: Request):
+    with open("login.html", "r") as f:
+        data = f.read()
+    return HTMLResponse(data)
+
+@app.post("/login")
+async def post_login(req: Request):
+  data = await req.body()
+  print(data)
